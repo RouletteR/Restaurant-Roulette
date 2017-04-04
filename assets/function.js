29c10ea74f3e2e10f42f7479d3170af4aed95e06
Elliott Chalmers <elliottchalmers@Elliotts-MacBook-Pro.local>
2017-04-03 20:56:39 -0400

    spin

--- assets/function.js
+++ assets/function.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
     var rouletteSvg = $(".svg").removeClass("hidden");
     $(".s12").prepend(rouletteSvg);
 
-
+    //Spin Wheel
+    $("#spin").on("click", function() {
+        var rotation = Math.floor(Math.random() * (1440 - 360) + 360);
+        $('.svg').velocity({ rotateZ: "+=" + rotation }, { duration: 3000, easing: "linear", loop: false });
+    });
 });
