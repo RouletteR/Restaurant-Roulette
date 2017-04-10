eac9aafef5b840f18ebbe5c149d7ad89c0f555c9
Alex Lovell <alexlovell@MacBook-Pro.local>
2017-04-09 20:38:52 -0400

    google

--- assets/function.js
+++ assets/function.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
 
-    
+function onSuccess(googleUser) {
+      console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    }
+    function onFailure(error) {
+      console.log(error);
+    }
+    function renderButton() {
+      gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+      });
+    } 
 
   // Initialize Firebase
   var config = {
@@ -177,7 +193,7 @@ $(document).ready(function() {
 
 
         $.ajax({
-                url: zQueryURL,
+                url: zQueryUrl,
                 beforeSend: function(request) {
                     request.setRequestHeader("user-key", zomatoToken);
                 },
