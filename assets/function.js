ed9d3617f792116e67f7664987444bb9ef82062a
Elliott Chalmers <elliottchalmers@elliotts-mbp.attlocal.net>
2017-04-10 08:22:01 -0400

    stuff

--- assets/function.js
+++ assets/function.js
@@ -310,7 +310,7 @@ $(document).ready(function() {
                 //}
                 // google.maps.event.addDomListener(window, 'load', initialize);
 
-                console.log(marker.setPosition);
+
 
                 //}
 
