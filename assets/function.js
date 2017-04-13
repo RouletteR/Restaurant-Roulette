8b9ab5fb719b03a241f0a19c9f2b6fc1a34dbcda
Elliott Chalmers <elliottchalmers@Elliotts-MacBook-Pro.local>
2017-04-12 21:44:18 -0400

    mo

--- assets/function.js
+++ assets/function.js
@@ -162,9 +162,9 @@ $(document).ready(function() {
     //Spin Wheel
     $(".spin").on("click", function() {
         var rotation = Math.floor(Math.random() * (1440 - 360) + 360);
-        $("#Layer_1").velocity({ rotateZ: rotation }, { duration: 3000, easing: "linear", loop: false })
+        $("#Layer_1").velocity({ rotateZ: [360, 0] }, { duration: 2500, easing: "linear", loop: false })
             //$("#Layer_1").velocity({ opacity: 0 }, { display: "none" }, {duration: 10000, easing: "linear"});
-            .velocity({ rotateY: ["-180deg", 0] }, { duration: 3000, loop: 0.5 });
+            .velocity({ rotateY: ["-360deg", 0] }, { duration: 500, easing: "exponential", loop: 0.5 });
     });
 
     function initialize() {
